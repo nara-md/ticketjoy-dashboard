bab6312b43a48c4918bfa346fde4bbd426ea89c8
Nara Mendes <283532505+nara-md@users.noreply.github.com>
2026-07-06 19:20:42 -0300

    feat: enhance ticket management with filter buttons and add new ticket data

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
 import tickets from './tickets.json' with { type: 'json' };
 
 const ticketList = document.getElementById('ticketList');
+const allticketsButton = document.getElementById('allTickets');
+const openTicketsButton = document.getElementById('openTickets');
+const closedTicketsButton = document.getElementById('closedTickets');
 
-tickets.forEach(ticket => {
-    const item = document.createElement('li');
-    const id = document.createElement('span');
-    id.textContent = ticket.id;
-    const user = document.createElement('span');
-    user.textContent = ticket.user;
-    const category = document.createElement('span');
-    category.textContent = ticket.category;
-    const priority = document.createElement('span');
-    priority.textContent = ticket.priority;
-    const status = document.createElement('span');
-    status.textContent = ticket.status;
+function renderTickets(tickets) {
+    ticketList.innerHTML = '';
+    ticketList.style.display = 'block';
 
-    item.appendChild(id);
-    item.appendChild(document.createElement('br'));
-    item.appendChild(user);
-    item.appendChild(document.createElement('br'));
-    item.appendChild(category);
-    item.appendChild(document.createElement('br'));
-    item.appendChild(priority);
-    item.appendChild(document.createElement('br'));
-    item.appendChild(status);
+    tickets.forEach(ticket => {
+        const item = document.createElement('li');
+        const id = document.createElement('span');
+        id.textContent = ticket.id;
+        const user = document.createElement('span');
+        user.textContent = ticket.user;
+        const category = document.createElement('span');
+        category.textContent = ticket.category;
+        const priority = document.createElement('span');
+        priority.textContent = ticket.priority;
+        const status = document.createElement('span');
+        status.textContent = ticket.status;
 
-    ticketList.appendChild(item);
-});
+        item.appendChild(id);
+        item.appendChild(document.createElement('br'));
+        item.appendChild(user);
+        item.appendChild(document.createElement('br'));
+        item.appendChild(category);
+        item.appendChild(document.createElement('br'));
+        item.appendChild(priority);
+        item.appendChild(document.createElement('br'));
+        item.appendChild(status);
+
+        ticketList.appendChild(item);
+    });
+}
+
+    function filterTicketsByStatus(status) {
+        const filteredTickets = tickets.filter(ticket => ticket.status.toLocaleLowerCase() === status.toLocaleLowerCase());
+        renderTickets(filteredTickets);
+    };
+
+    allticketsButton.addEventListener('click', () => {
+        renderTickets(tickets);
+    });
+
+    openTicketsButton.addEventListener('click', () => {
+        filterTicketsByStatus('aberto');
+    });
+
+    closedTicketsButton.addEventListener('click', () => {
+        filterTicketsByStatus('fechado');
+    });
 
 // console.log(tickets);
+
